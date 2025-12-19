a8c5149ab5e23c9f4d9add81768e961bd1e805d5
eslam985 <eslam985@users.noreply.github.com>
2025-12-19 03:50:25 +0000

    deploy: 6261cbc922cbcca59f2bb04c3a1372f1d8659a88

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3236848782"
+    serviceWorkerVersion: "210596628"
   }
 });
